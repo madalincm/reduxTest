141ec6ea24d231fea7b0c0d3f0b8584f4ef2c6fb
Madalin Cotetiu <cotetiumadalin@gmail.com>
2018-12-24 14:07:00 +0200

    refactoring

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ 'description':'water bill', 'amount':4000, createdAt: 1002 }))
-store.dispatch(addExpense({ 'description':'gas bill', 'amount':9000, createdAt:1000 }))
-store.dispatch(addExpense({ 'description':'rent', 'amount':1924000, createdAt: 1001 }))
-console.log(store.getState());
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
